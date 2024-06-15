f166e0165ba61a40dfe779c3aeaac367e6a436af
Andrew Bates <a@sprock.io>
2024-06-14 21:20:51 -0500

    Add logger to func signatures

--- server/evr_runtime_discord_registry.go
+++ server/evr_runtime_discord_registry.go
@@ -459,9 +459,9 @@ func (r *LocalDiscordRegistry) UpdateAccount(ctx context.Context, userID uuid.UU
 	timer := time.Now()
 	if r.metrics != nil {
 
-		defer func() { logger.Debug("UpdateAccount took %dms", time.Since(timer)*time.Millisecond) }()
+		defer func() { logger.Debug("UpdateAccount took %dms", time.Since(timer)/time.Millisecond) }()
 		defer func() {
-			r.metrics.CustomTimer("UpdateAccountFn_duration_timer", nil, time.Since(timer)*time.Millisecond)
+			r.metrics.CustomTimer("UpdateAccountFn_duration_ms_timer", nil, time.Since(timer)/time.Millisecond)
 		}()
 	}
 
