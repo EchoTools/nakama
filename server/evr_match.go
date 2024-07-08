cd4e9369a5fac8a8a3b25b8751c4eb6f628b7588
Andrew Bates <a@sprock.io>
2024-07-08 09:15:53 -0500

    Combine and simplify backfill loop

--- server/evr_match.go
+++ server/evr_match.go
@@ -27,8 +27,7 @@ const (
 	VersionLock       uint64 = 0xc62f01d78f77910d // The game build version.
 	MatchmakingModule        = "evr"              // The module used for matchmaking
 
-	MatchMaxSize                             = 14 // The total max players (not including the broadcaster) for a EVR lobby.
-	MatchMaxSocialSize                       = 12 // The total max players (not including the broadcaster) for a EVR social lobby.
+	MatchMaxSize                             = 12 // The total max players (not including the broadcaster) for a EVR lobby.
 	LevelSelectionFirst  MatchLevelSelection = "first"
 	LevelSelectionRandom MatchLevelSelection = "random"
 
