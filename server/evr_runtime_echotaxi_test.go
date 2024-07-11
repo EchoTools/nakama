e7c8e515b8e6f6b980116945ce95c010f6d7f957
Andrew Bates <a@sprock.io>
2024-07-10 22:55:14 -0500

    Refactor link parsing in EchoTaxi

--- server/evr_runtime_echotaxi_test.go
+++ server/evr_runtime_echotaxi_test.go
@@ -2,55 +2,11 @@ package server
 
 import (
 	"context"
-	"sync"
 	"testing"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/heroiclabs/nakama-common/runtime"
 	"go.uber.org/zap"
 )
-
-func TestTaxiLinkRegistry_Process(t *testing.T) {
-	t.Skip("Skipping test for now")
-	type fields struct {
-		Mutex       sync.Mutex
-		ctx         context.Context
-		ctxCancelFn context.CancelFunc
-		nk          runtime.NakamaModule
-		logger      runtime.Logger
-		dg          *discordgo.Session
-		node        string
-	}
-	type args struct {
-		channelID string
-		messageID string
-		content   string
-		reactOnly bool
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			e := &TaxiLinkRegistry{
-				ctx:         tt.fields.ctx,
-				ctxCancelFn: tt.fields.ctxCancelFn,
-				nk:          tt.fields.nk,
-				logger:      tt.fields.logger,
-				dg:          tt.fields.dg,
-				node:        tt.fields.node,
-			}
-			if err := e.Process(tt.args.channelID, tt.args.messageID, tt.args.content, tt.args.reactOnly); (err != nil) != tt.wantErr {
-				t.Errorf("TaxiLinkRegistry.Process() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
 
 func createTestTaxiBot(t *testing.T, logger *zap.Logger) (*TaxiBot, error) {
 	ctx := context.Background()
@@ -120,62 +76,62 @@ func Test_extractMatchComponents(t *testing.T) {
 			args:              args{content: "https://echo.taxi/spark://j/c74b6b7a-b77f-460e-baac-e2f1437001d9"},
 			wantHttpPrefix:    "https://echo.taxi/",
 			wantApplinkPrefix: "spark://j/",
-			wantMatchID:       "c74b6b7a-b77f-460e-baac-e2f1437001d9",
+			wantMatchID:       "c74b6b7a-b77f-460e-baac-e2f1437001d9.test",
 		},
 		{
 			name:              "Test Spark Link prefixed with sprock.io",
 			args:              args{content: "https://sprock.io/spark://j/c74b6b7a-b77f-460e-baac-e2f1437001d9"},
 			wantHttpPrefix:    "https://sprock.io/",
 			wantApplinkPrefix: "spark://j/",
-			wantMatchID:       "c74b6b7a-b77f-460e-baac-e2f1437001d9",
+			wantMatchID:       "c74b6b7a-b77f-460e-baac-e2f1437001d9.test",
 		},
 		{
 			name:              "Test Spark Link with lowercase",
 			args:              args{content: "spark://j/c74b6b7a-b77f-460e-baac-e2f1437001d9"},
 			wantHttpPrefix:    "",
 			wantApplinkPrefix: "spark://j/",
-			wantMatchID:       "c74b6b7a-b77f-460e-baac-e2f1437001d9",
+			wantMatchID:       "c74b6b7a-b77f-460e-baac-e2f1437001d9.test",
 		},
 		{
 			name:              "Test Spark Link (Choose)",
 			args:              args{content: "spark://c/c74b6b7a-b77F-460E-BAAC-E2F1437001D9"},
 			wantHttpPrefix:    "",
 			wantApplinkPrefix: "spark://c/",
-			wantMatchID:       "c74b6b7a-b77f-460e-baac-e2f1437001d9",
+			wantMatchID:       "c74b6b7a-b77f-460e-baac-e2f1437001d9.test",
 		},
 		{
 			name:              "Test Spark Link (Join)",
 			args:              args{content: "spark://j/C74B6B7A-B77F-460E-BAAC-E2F1437001D9"},
 			wantHttpPrefix:    "",
 			wantApplinkPrefix: "spark://j/",
-			wantMatchID:       "c74b6b7a-b77f-460e-baac-e2f1437001d9",
+			wantMatchID:       "c74b6b7a-b77f-460e-baac-e2f1437001d9.test",
 		},
 		{
 			name:              "Test Spark Link (Spectate)",
 			args:              args{content: "spark://s/c74b6b7a-b77F-460E-BAAC-E2F1437001D9"},
 			wantHttpPrefix:    "",
 			wantApplinkPrefix: "spark://s/",
-			wantMatchID:       "c74b6b7a-b77f-460e-baac-e2f1437001d9",
+			wantMatchID:       "c74b6b7a-b77f-460e-baac-e2f1437001d9.test",
 		},
 		{
 			name:              "Test Aether link",
 			args:              args{content: "Aether://C74B6B7A-B77F-460E-BAAC-E2F1437001D9"},
 			wantHttpPrefix:    "",
 			wantApplinkPrefix: "Aether://",
-			wantMatchID:       "c74b6b7a-b77f-460e-baac-e2f1437001d9",
+			wantMatchID:       "c74b6b7a-b77f-460e-baac-e2f1437001d9.test",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotHttpPrefix, gotApplinkPrefix, gotMatchID := extractMatchComponents(tt.args.content)
-			if gotHttpPrefix != tt.wantHttpPrefix {
-				t.Errorf("extractMatchComponents() gotHttpPrefix = %v, want %v", gotHttpPrefix, tt.wantHttpPrefix)
+			matchLink := (&TaxiBot{node: "test"}).matchLinkFromStringOrNil(tt.args.content)
+			if matchLink.HTTPPrefix != tt.wantHttpPrefix {
+				t.Errorf("extractMatchComponents() gotHttpPrefix = %v, want %v", matchLink.HTTPPrefix, tt.wantHttpPrefix)
 			}
-			if gotApplinkPrefix != tt.wantApplinkPrefix {
-				t.Errorf("extractMatchComponents() gotApplinkPrefix = %v, want %v", gotApplinkPrefix, tt.wantApplinkPrefix)
+			if matchLink.AppLinkPrefix != tt.wantApplinkPrefix {
+				t.Errorf("extractMatchComponents() gotApplinkPrefix = %v, want %v", matchLink.AppLinkPrefix, tt.wantApplinkPrefix)
 			}
-			if gotMatchID != tt.wantMatchID {
-				t.Errorf("extractMatchComponents() gotMatchID = %v, want %v", gotMatchID, tt.wantMatchID)
+			if matchLink.MatchID != MatchIDFromStringOrNil(tt.wantMatchID) {
+				t.Errorf("extractMatchComponents() gotMatchID = %v, want %v", matchLink.MatchID.String(), tt.wantMatchID)
 			}
 		})
 	}
