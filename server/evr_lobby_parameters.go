7136490ddbb7b941044713c4093527075031c799
Andrew Bates <a@sprock.io>
2024-08-29 01:30:11 -0500

    Add avoid blocked players feature

--- server/evr_lobby_parameters.go
+++ server/evr_lobby_parameters.go
@@ -2,11 +2,12 @@ package server
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/gofrs/uuid/v5"
+	"github.com/heroiclabs/nakama-common/api"
 	"github.com/heroiclabs/nakama/v3/server/evr"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 type (
@@ -14,54 +15,38 @@ type (
 )
 
 type SessionParameters struct {
-	Node                  string     `json:"node"`
-	VersionLock           evr.Symbol `json:"version_lock"`
-	AppID                 evr.Symbol `json:"app_id"`
-	GroupID               uuid.UUID  `json:"group_id"`
-	Region                evr.Symbol `json:"region"`
-	Mode                  evr.Symbol `json:"mode"`
-	Level                 evr.Symbol `json:"level"`
-	SupportedFeatures     []string   `json:"supported_features"`
-	RequiredFeatures      []string   `json:"required_features"`
-	CurrentMatchID        MatchID    `json:"current_match_id"`
-	Role                  int        `json:"role"`
-	PartyID               uuid.UUID  `json:"party_id"`
-	PartyGroupID          string     `json:"party_group_id"`
-	PartySize             int        `json:"party_size"`
-	NextMatchID           MatchID    `json:"next_match_id"`
-	DisableArenaBackfill  bool       `json:"disable_arena_backfill"`
-	BackfillQueryAddon    string     `json:"backfill_query_addon"`
-	MatchmakingQueryAddon string     `json:"matchmaking_query_addon"`
-	CreateQueryAddon      string     `json:"create_query_addon"`
-	Verbose               bool       `json:"verbose"`
+	Node                  string             `json:"node"`
+	VersionLock           evr.Symbol         `json:"version_lock"`
+	AppID                 evr.Symbol         `json:"app_id"`
+	GroupID               uuid.UUID          `json:"group_id"`
+	Region                evr.Symbol         `json:"region"`
+	Mode                  evr.Symbol         `json:"mode"`
+	Level                 evr.Symbol         `json:"level"`
+	SupportedFeatures     []string           `json:"supported_features"`
+	RequiredFeatures      []string           `json:"required_features"`
+	CurrentMatchID        MatchID            `json:"current_match_id"`
+	Role                  int                `json:"role"`
+	PartyID               uuid.UUID          `json:"party_id"`
+	PartyGroupID          string             `json:"party_group_id"`
+	PartySize             int                `json:"party_size"`
+	NextMatchID           MatchID            `json:"next_match_id"`
+	DisableArenaBackfill  bool               `json:"disable_arena_backfill"`
+	BackfillQueryAddon    string             `json:"backfill_query_addon"`
+	StringProperties      map[string]string  `json:"string_properties"`
+	NumericProperties     map[string]float64 `json:"numeric_properties"`
+	MatchmakingQueryAddon string             `json:"matchmaking_query_addon"`
+	CreateQueryAddon      string             `json:"create_query_addon"`
+	Verbose               bool               `json:"verbose"`
+	Friends               []*api.Friend      `json:"friends"`
 	latencyHistory        LatencyHistory
 }
 
-func (s SessionParameters) ResponseFromError(err error) *evr.LobbySessionFailurev4 {
+func QueryEscapeUUID(userID uuid.UUID) string {
+	return strings.ReplaceAll(userID.String(), "-", "\\-")
+}
 
-	respCode := evr.LobbySessionFailure_InternalError
-	message := err.Error()
-	if s, ok := status.FromError(err); ok {
-		switch s.Code() {
-		case codes.DeadlineExceeded:
-			respCode = evr.LobbySessionFailure_Timeout0
-		case codes.InvalidArgument:
-			respCode = evr.LobbySessionFailure_BadRequest
-		case codes.Aborted:
-			respCode = evr.LobbySessionFailure_BadRequest
-		case codes.ResourceExhausted:
-			respCode = evr.LobbySessionFailure_ServerIsFull
-		case codes.Unavailable:
-			respCode = evr.LobbySessionFailure_ServerFindFailed
-		case codes.NotFound:
-			respCode = evr.LobbySessionFailure_ServerDoesNotExist
-		case codes.PermissionDenied, codes.Unauthenticated:
-			respCode = evr.LobbySessionFailure_KickedFromLobbyGroup
-		case codes.FailedPrecondition:
-			respCode = evr.LobbySessionFailure_ServerIsIncompatible
-		}
-	}
-	return evr.NewLobbySessionFailure(s.Mode, s.GroupID, respCode, message).Version4()
+func QueryEscapeUUIDStr(userID string) string {
+	return strings.ReplaceAll(userID, "-", "\\-")
 }
 
 func (s SessionParameters) MetricsTags() map[string]string {
@@ -74,7 +59,7 @@ func (s SessionParameters) MetricsTags() map[string]string {
 	}
 }
 
-func NewLobbyParametersFromRequest(ctx context.Context, r evr.LobbySessionRequest, gconfig MatchmakingSettings, config MatchmakingSettings, latencyHistory LatencyHistory) SessionParameters {
+func NewLobbyParametersFromRequest(ctx context.Context, r evr.LobbySessionRequest, globalSettings *MatchmakingSettings, userSettings *MatchmakingSettings, profile *GameProfileData, latencyHistory LatencyHistory, friends []*api.Friend) SessionParameters {
 	node := ctx.Value(ctxNodeKey{}).(string)
 	requiredFeatures, ok := ctx.Value(ctxRequiredFeaturesKey{}).([]string)
 	if !ok {
@@ -105,9 +90,55 @@ func NewLobbyParametersFromRequest(ctx context.Context, r evr.LobbySessionReques
 	if r.GetCurrentLobbyID() != uuid.Nil {
 		currentMatchID = MatchID{UUID: r.GetCurrentLobbyID(), Node: node}
 	}
-	lobbyGroupID := config.LobbyGroupID
+	lobbyGroupID := userSettings.LobbyGroupID
 	if lobbyGroupID == "" {
 		lobbyGroupID = uuid.Must(uuid.NewV4()).String()
+	}
+
+	userID, ok := ctx.Value(ctxUserIDKey{}).(uuid.UUID)
+	if !ok {
+		userID = uuid.Nil
+	}
+
+	if globalSettings == nil {
+		globalSettings = &MatchmakingSettings{}
+	}
+
+	if userSettings == nil {
+		userSettings = &MatchmakingSettings{}
+	}
+	// Add blocked players who are online to the backfillQueryAddon
+	stringProperties := make(map[string]string)
+
+	blockedFriendIDs := make([]string, 0)
+	for _, f := range friends {
+		if api.Friend_State(f.GetState().Value) == api.Friend_BLOCKED {
+			blockedFriendIDs = append(blockedFriendIDs, f.GetUser().GetId())
+		}
+	}
+	if len(blockedFriendIDs) > 0 {
+		stringProperties["blocked_friend_ids"] = strings.Join(blockedFriendIDs, " ")
+	}
+
+	// Escape -'s in the user id
+	userIDStr := QueryEscapeUUID(userID)
+
+	matchmakingQueryAddons := []string{
+		globalSettings.MatchmakingQueryAddon,
+		userSettings.MatchmakingQueryAddon,
+		fmt.Sprintf(`-properties.blocked:/%s/`, userIDStr),
+	}
+
+	backfillQueryAddons := []string{
+		globalSettings.BackfillQueryAddon,
+		userSettings.BackfillQueryAddon,
+	}
+	// Add each blocked user that is online to the backfill query addon
+	for _, f := range friends {
+		if api.Friend_State(f.GetState().Value) == api.Friend_BLOCKED && f.GetUser().GetOnline() {
+			userIDStr := QueryEscapeUUIDStr(f.GetUser().GetId())
+			backfillQueryAddons = append(backfillQueryAddons, fmt.Sprintf(`-label.players.user_id:/%s/`, userIDStr))
+		}
 	}
 
 	return SessionParameters{
@@ -121,14 +152,14 @@ func NewLobbyParametersFromRequest(ctx context.Context, r evr.LobbySessionReques
 		SupportedFeatures:     supportedFeatures,
 		RequiredFeatures:      requiredFeatures,
 		Role:                  r.GetEntrantRole(0),
-		DisableArenaBackfill:  gconfig.DisableArenaBackfill || config.DisableArenaBackfill,
-		BackfillQueryAddon:    gconfig.BackfillQueryAddon + " " + config.BackfillQueryAddon,
-		MatchmakingQueryAddon: gconfig.MatchmakingQueryAddon + " " + config.MatchmakingQueryAddon,
-		CreateQueryAddon:      gconfig.CreateQueryAddon + " " + config.CreateQueryAddon,
+		DisableArenaBackfill:  globalSettings.DisableArenaBackfill || userSettings.DisableArenaBackfill,
+		BackfillQueryAddon:    strings.Join(backfillQueryAddons, " "),
+		MatchmakingQueryAddon: strings.Join(matchmakingQueryAddons, " "),
+		CreateQueryAddon:      globalSettings.CreateQueryAddon + " " + userSettings.CreateQueryAddon,
 		PartyGroupID:          lobbyGroupID,
 		PartyID:               uuid.NewV5(uuid.Nil, lobbyGroupID),
-		NextMatchID:           config.NextMatchID,
-		Verbose:               gconfig.Verbose || config.Verbose,
+		NextMatchID:           userSettings.NextMatchID,
+		Verbose:               globalSettings.Verbose || userSettings.Verbose,
 		Node:                  node,
 		latencyHistory:        latencyHistory,
 	}
