ee987dddc5e033bbfd17f41e166d865d59b7e018
Andrew Bates <a@sprock.io>
2024-09-10 00:26:31 -0500

    Send matchmaking status to stream

--- server/evr_lobby_status.go
+++ server/evr_lobby_status.go
@@ -4,36 +4,32 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/heroiclabs/nakama-common/rtapi"
 	"go.uber.org/zap"
 )
+
+type MatchmakingStreamOpCode uint8
+
+const (
+	MatchmakingStreamOpCodeParameters MatchmakingStreamOpCode = iota
+)
+
+type MatchmakingStreamData struct {
+	Parameters *LobbySessionParameters `json:"parameters,omitempty"`
+}
+
+func (d MatchmakingStreamData) String() string {
+	b, _ := json.Marshal(d)
+	return string(b)
+}
 
 type GuildLobbyLabel struct {
 	GroupID string `json:"group_id"`
 }
 
-type MatchmakingGroupLabel struct {
-	Mode        string `json:"mode"`
-	GroupID     string `json:"group_id"`
-	VersionLock string `json:"version_lock"`
-}
-
-func (l MatchmakingGroupLabel) String() string {
-	data, _ := json.Marshal(l)
-	return string(data)
-}
-
 func JoinMatchmakingStream(logger *zap.Logger, s *sessionWS, params *LobbySessionParameters) (PresenceStream, error) {
 
-	label := MatchmakingGroupLabel{
-		Mode:        params.Mode.String(),
-		GroupID:     params.GroupID.String(),
-		VersionLock: params.VersionLock.String(),
-	}
-
-	labelStr := label.String()
-	_ = labelStr
-
-	groupStream := PresenceStream{Mode: StreamModeMatchmaking, Subject: params.GroupID, Label: label.String()}
+	groupStream := PresenceStream{Mode: StreamModeMatchmaking, Subject: params.GroupID}
 	logger.Debug("Joining lobby group matchmaking stream", zap.Any("stream", groupStream))
 
 	data, err := json.Marshal(params)
@@ -46,18 +42,32 @@ func JoinMatchmakingStream(logger *zap.Logger, s *sessionWS, params *LobbySessio
 	}
 
 	// Leave any existing lobby group stream.
-	s.tracker.UntrackLocalByModes(s.id, map[uint8]struct{}{StreamModeMatchmaking: {}}, PresenceStream{})
+	s.tracker.UntrackLocalByModes(s.id, map[uint8]struct{}{StreamModeMatchmaking: {}}, groupStream)
 
 	ctx := s.Context()
 
-	if success := s.tracker.TrackMulti(ctx, s.id, []*TrackerOp{
-		{Stream: groupStream, Meta: presenceMeta},
-	}, s.userID); !success {
-
+	if success := s.tracker.Update(ctx, s.id, groupStream, s.userID, presenceMeta); !success {
 		return PresenceStream{}, fmt.Errorf("failed to track lobby group matchmaking stream")
 	} else {
 		logger.Debug("Tracked lobby group matchmaking stream", zap.Any("stream", groupStream), zap.Any("meta", presenceMeta))
 	}
+
+	s.pipeline.router.SendToStream(logger, groupStream, &rtapi.Envelope{
+		Message: &rtapi.Envelope_StreamData{
+			StreamData: &rtapi.StreamData{
+				Stream: &rtapi.Stream{
+					Mode:    StreamModeMatchmaking,
+					Subject: groupStream.Subject.String(),
+				},
+				Sender: &rtapi.UserPresence{
+					UserId:    s.UserID().String(),
+					SessionId: s.ID().String(),
+					Username:  s.Username(),
+				},
+				Data: MatchmakingStreamData{Parameters: params}.String(),
+			},
+		},
+	}, true)
 
 	// Track the groupID as well
 	return groupStream, nil
