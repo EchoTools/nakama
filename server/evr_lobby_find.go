d4ba87949a783272f13b5e91fd6f7945af09e1c1
Andrew Bates <a@sprock.io>
2024-12-04 02:40:40 +0000

    Increase fallback time to 2x ticket timeout

--- server/evr_lobby_find.go
+++ server/evr_lobby_find.go
@@ -314,7 +314,7 @@ func (p *EvrPipeline) lobbyBackfill(ctx context.Context, logger *zap.Logger, lob
 
 	cycleCount := 0
 
-	fallbackTimer := time.NewTimer(p.matchmakingTicketTimeout())
+	fallbackTimer := time.NewTimer(p.matchmakingTicketTimeout() * 2)
 
 	for {
 
