44a9ee0a58dda53f2231d8202b4db50a25bd443f
Andrew Bates <a@sprock.io>
2025-02-06 01:54:27 +0000

    Add developer applications support

---
+++ server/evr_developer_applications.go
@@ -0,0 +1,128 @@
+package server
+
+import (
+	"context"
+	"crypto"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/gofrs/uuid/v5"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/heroiclabs/nakama-common/runtime"
+)
+
+var _ = Storable(&DeveloperApplications{})
+
+type DeveloperApplications struct {
+	Applications []DeveloperApplication `json:"applications"`
+}
+
+func (DeveloperApplications) StorageID() StorageID {
+	return StorageID{
+		Collection: "Developer",
+		Key:        "applications",
+	}
+}
+
+func (DeveloperApplications) StorageIndex() *StorageIndexMeta {
+	return &StorageIndexMeta{
+		Name:       "developerApplicationTokens",
+		Collection: "Developer",
+		Key:        "applications",
+		Fields:     []string{"value.applications.token"},
+		MaxEntries: 10000,
+		IndexOnly:  true,
+	}
+}
+
+type DeveloperApplication struct {
+	ID            uuid.UUID `json:"id"`
+	Name          string    `json:"name"`
+	Description   string    `json:"description"`
+	Token         string    `json:"token"`
+	TokenIssuedAt time.Time `json:"token_issued_at"`
+}
+
+func NewDeveloperApplicationToken(app DeveloperApplication, signingKey string) string {
+	tokenID := uuid.Must(uuid.NewV4()).String()
+
+	token, _ := generateDeveloperTokenWithExpiry(signingKey, app.ID.String(), tokenID, app.TokenIssuedAt.Unix(), "", "", nil, time.Now().Add(time.Hour*24*365*10))
+	return token
+}
+
+func ApplicationTokenAuthenticate(ctx context.Context, nk runtime.NakamaModule, token string) (string, error) {
+	idx := DeveloperApplications{}.StorageIndex()
+	query := fmt.Sprintf("+value.applications.token:%s", Query.Escape(token))
+	objs, _, err := nk.StorageIndexList(ctx, SystemUserID, idx.Name, query, 1, nil, "")
+	if err != nil {
+		return "", err
+	}
+	if len(objs.Objects) == 0 {
+		return "", nil
+	}
+
+	return objs.Objects[0].UserId, nil
+}
+
+type ApplicationTokenClaims struct {
+	TokenID       string            `json:"tid,omitempty"`
+	UserID        string            `json:"uid,omitempty"`
+	ApplicationID string            `json:"aid,omitempty"`
+	Username      string            `json:"usn,omitempty"`
+	Vars          map[string]string `json:"vrs,omitempty"`
+	ExpiresAt     int64             `json:"exp,omitempty"`
+	IssuedAt      int64             `json:"iat,omitempty"`
+}
+
+func (stc *ApplicationTokenClaims) Valid() error {
+	// Verify expiry.
+	if stc.ExpiresAt <= time.Now().UTC().Unix() {
+		vErr := new(jwt.ValidationError)
+		vErr.Inner = errors.New("Token is expired")
+		vErr.Errors |= jwt.ValidationErrorExpired
+		return vErr
+	}
+	return nil
+}
+
+func parseApplicationToken(hmacSecretByte []byte, tokenString string) (applicationID uuid.UUID, userID uuid.UUID, username string, vars map[string]string, exp int64, tokenID string, issuedAt int64, ok bool) {
+	jwtToken, err := jwt.ParseWithClaims(tokenString, &ApplicationTokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if s, ok := token.Method.(*jwt.SigningMethodHMAC); !ok || s.Hash != crypto.SHA256 {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return hmacSecretByte, nil
+	})
+	if err != nil {
+		return
+	}
+	claims, ok := jwtToken.Claims.(*ApplicationTokenClaims)
+	if !ok || !jwtToken.Valid {
+		return
+	}
+	applicationID, err = uuid.FromString(claims.ApplicationID)
+	if err != nil {
+		return
+	}
+
+	userID, err = uuid.FromString(claims.UserID)
+	if err != nil {
+		return
+	}
+	return applicationID, userID, claims.Username, claims.Vars, claims.ExpiresAt, claims.TokenID, claims.IssuedAt, true
+}
+
+func generateDeveloperTokenWithExpiry(signingKey, applicationID string, tokenID string, tokenIssuedAt int64, userID, username string, vars map[string]string, expiry time.Time) (string, int64) {
+	exp := expiry.UTC().Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &ApplicationTokenClaims{
+		TokenID:       tokenID,
+		ApplicationID: applicationID,
+		UserID:        userID,
+		Username:      username,
+		Vars:          vars,
+		ExpiresAt:     exp,
+		IssuedAt:      tokenIssuedAt,
+	})
+	signedToken, _ := token.SignedString([]byte(signingKey))
+	return signedToken, exp
+}
--- server/evr_runtime_api.go
+++ server/evr_runtime_api.go
@@ -3,13 +3,99 @@ package server
 import (
 	"context"
 	"database/sql"
+	"net"
+	"net/http"
+	"strings"
 
 	"github.com/heroiclabs/nakama-common/runtime"
 )
 
-type APIServer struct{}
+const (
+	DEVAPP_CTX_APP_ID = "dev_app_id"
+)
 
-func (s *APIServer) RPCHandler(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
-	// Unmarshal the request payload.
-	return "", nil
+type AppAPI struct {
+	ctx      context.Context
+	logger   runtime.Logger
+	db       *sql.DB
+	nk       runtime.NakamaModule
+	handlers map[string]func(context.Context, http.ResponseWriter, *http.Request)
+}
+
+func NewAppAPI(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, initializer runtime.Initializer) *AppAPI {
+	return &AppAPI{
+		ctx:    ctx,
+		logger: logger,
+		db:     db,
+		nk:     nk,
+	}
+}
+
+func NewAppAPIAcceptor(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, initializer runtime.Initializer) func(http.ResponseWriter, *http.Request) {
+	appAPI := NewAppAPI(ctx, logger, db, nk, initializer)
+
+	_nk := nk.(*RuntimeGoNakamaModule)
+	signingKey := _nk.config.GetSession().EncryptionKey
+	config := _nk.config
+	node := config.GetName()
+	env := config.GetRuntime().Environment
+
+	// This handler will be attached to the API Gateway server.
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		// Check authentication.
+		var token string
+		if auth := r.Header["Authorization"]; len(auth) >= 1 {
+			// Attempt header based authentication.
+			const prefix = "Bearer "
+			if !strings.HasPrefix(auth[0], prefix) {
+				http.Error(w, "Missing or invalid token", 401)
+				return
+			}
+			token = auth[0][len(prefix):]
+		} else {
+			// Attempt query parameter based authentication.
+			token = r.URL.Query().Get("token")
+		}
+		if token == "" {
+			http.Error(w, "Missing or invalid token", 401)
+			return
+		}
+
+		applicationID, userID, username, vars, _, _, _, ok := parseApplicationToken([]byte(signingKey), token)
+		if !ok {
+			http.Error(w, "Missing or invalid token", 401)
+			return
+		}
+
+		// Parse out the IP and port (IP:port) from the RemoteAddr
+		clientIP, clientPort, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			clientIP = r.RemoteAddr
+			clientPort = ""
+		}
+
+		ctx := NewDeveloperAppContext(r.Context(), node, "", env, r.Header, r.URL.Query(), userID.String(), username, vars, clientIP, clientPort, applicationID.String())
+
+		// Get the api path from the request
+		path := r.URL.Path
+
+		// Get the handler function for the api path
+		handlerFn, ok := appAPI.handlers[path]
+		if !ok {
+			http.Error(w, "Not Found", 404)
+			return
+		}
+
+		// Call the handler function
+		handlerFn(ctx, w, r)
+	}
+}
+
+// func(http.ResponseWriter, *http.Request) handler
+func (api *AppAPI) handlerFn(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+
+	// Respond to the request
+	w.Write([]byte("Hello, World!"))
+
 }
