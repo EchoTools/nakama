8646a5901a12d976b90f0c3f70b17314605edb99
Andrew Bates <a@sprock.io>
2025-02-27 22:35:57 +0000

    Fix game pause settings in metadata

--- server/evr_discord_appbot.go
+++ server/evr_discord_appbot.go
@@ -1846,17 +1846,17 @@ func (d *DiscordAppBot) RegisterSlashCommands() error {
 				Fields: []*discordgo.MessageEmbedField{
 					{
 						Name:   "Grab Deadzone",
-						Value:  strconv.FormatFloat(metadata.GameSettings.Grabdeadzone, 'f', -1, 64),
+						Value:  strconv.FormatFloat(metadata.GamePauseSettings.Grabdeadzone, 'f', -1, 64),
 						Inline: true,
 					},
 					{
 						Name:   "Release Distance",
-						Value:  strconv.FormatFloat(metadata.GameSettings.Releasedistance, 'f', -1, 64),
+						Value:  strconv.FormatFloat(metadata.GamePauseSettings.Releasedistance, 'f', -1, 64),
 						Inline: true,
 					},
 					{
 						Name:   "Wrist Angle Offset",
-						Value:  strconv.FormatFloat(metadata.GameSettings.Wristangleoffset, 'f', -1, 64),
+						Value:  strconv.FormatFloat(metadata.GamePauseSettings.Wristangleoffset, 'f', -1, 64),
 						Inline: true,
 					},
 				},
