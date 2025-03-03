0263b4c4e262a61af7343b402d93621effdfe246
Andrew Bates <a@sprock.io>
2025-03-03 01:07:36 +0000

    Add igp to priviledged commands

--- server/evr_discord_appbot_handlers.go
+++ server/evr_discord_appbot_handlers.go
@@ -89,7 +89,7 @@ func (d *DiscordAppBot) handleInteractionApplicationCommand(logger runtime.Logge
 			return simpleInteractionResponse(s, i, "You must be a guild allocator to use this command.")
 		}
 
-	case "trigger-cv", "kick-player", "join-player":
+	case "trigger-cv", "kick-player", "join-player", "igp":
 
 		if group.AuditChannelID != "" {
 			if err := d.LogInteractionToChannel(i, group.AuditChannelID); err != nil {
