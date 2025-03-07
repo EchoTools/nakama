da32b7e09aa013f558b9b01dd4c1b6ccdedf5828
Andrew Bates <a@sprock.io>
2025-03-07 13:57:03 -0600

    Add leaderboard haystack RPC

--- server/evr_runtime_rpc_stats.go
+++ server/evr_runtime_rpc_stats.go
@@ -10,162 +10,83 @@ import (
 	"github.com/heroiclabs/nakama/v3/server/evr"
 )
 
-type UserLeaderboardRecordsRequest struct {
-	UserID    string `json:"user_id"`
-	DiscordID string `json:"discord_id"`
-	GuildID   string `json:"guild_id"`
-	GroupID   string `json:"group_id"`
+type PlayerStatisticsRequest struct {
+	UserID    string     `json:"user_id"`
+	GroupID   string     `json:"group_id"`
+	GuildID   string     `json:"guild_id"`
+	DiscordID string     `json:"discord_id"`
+	Mode      evr.Symbol `json:"mode"`
 }
 
-func LeaderboardRecordsRPC(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
-	request := &UserLeaderboardRecordsRequest{}
+type PlayerStatisticsResponse struct {
+	Stats evr.PlayerStatistics `json:"stats"`
+}
+
+func (r *PlayerStatisticsResponse) String() string {
+	data, err := json.MarshalIndent(r, "", "  ")
+	if err != nil {
+		return ""
+	}
+	return string(data)
+}
+
+func PlayerStatisticsRPC(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
+	request := &PlayerStatisticsRequest{}
 	if err := parseRequest(ctx, payload, request); err != nil {
 		return "", err
 	}
 
-	var err error
-	if request.DiscordID != "" {
-		if request.UserID, err = GetUserIDByDiscordID(ctx, db, request.DiscordID); err != nil {
+	switch {
+	case request.UserID != "":
+	case request.DiscordID != "":
+		if userID, err := GetUserIDByDiscordID(ctx, db, request.DiscordID); err != nil {
 			return "", fmt.Errorf("failed to get user ID by discord ID: %w", err)
+		} else {
+			request.UserID = userID
 		}
 	}
 
 	if request.UserID == "" {
-		return "", runtime.NewError("No user ID specified", StatusInvalidArgument)
+		return "", runtime.NewError("user not found", StatusNotFound)
 	}
 
-	if request.GuildID != "" {
-		if request.GroupID, err = GetGroupIDByGuildID(ctx, db, request.GuildID); err != nil {
-			return "", fmt.Errorf("failed to get group ID by discord ID: %w", err)
+	switch {
+	case request.GroupID != "":
+	case request.GuildID != "":
+		if groupID, err := GetGroupIDByGuildID(ctx, db, request.GuildID); err != nil {
+			return "", fmt.Errorf("failed to get group ID by guild ID: %w", err)
+		} else {
+			request.GroupID = groupID
 		}
 	}
 
 	if request.GroupID == "" {
-		return "", runtime.NewError("No group ID specified", StatusInvalidArgument)
+		return "", runtime.NewError("guild group not found", StatusNotFound)
 	}
 
-	records, err := retrievePlayerLeaderboardRecords(ctx, db, request.UserID, request.GroupID)
+	var modes []evr.Symbol
+
+	if request.Mode.IsNil() {
+		modes = []evr.Symbol{
+			evr.ModeArenaPublic,
+			evr.ModeCombatPublic,
+			evr.ModeCombatPrivate,
+			evr.ModeArenaPrivate,
+			evr.ModeSocialPublic,
+			evr.ModeSocialPrivate,
+		}
+	} else {
+		modes = []evr.Symbol{request.Mode}
+	}
+
+	stats, _, err := PlayerStatisticsGetID(ctx, db, nk, request.UserID, request.GroupID, modes, request.Mode)
 	if err != nil {
 		return "", err
 	}
 
-	data, err := json.MarshalIndent(records, "", "  ")
-	if err != nil {
-		return "", fmt.Errorf("failed to marshal records: %w", err)
+	response := &PlayerStatisticsResponse{
+		Stats: stats,
 	}
 
-	return string(data), nil
-}
-
-func retrievePlayerLeaderboardRecords(ctx context.Context, db *sql.DB, userID string, groupID string) (map[evr.Symbol]map[string]map[evr.ResetSchedule]float64, error) {
-	query := `
-		SELECT 
-			leaderboard_id,
-			score,
-			subscore
-		FROM leaderboard_record
-		WHERE owner_id = $1
-		AND (expiry_time > NOW() OR expiry_time = '1970-01-01 00:00:00+00') -- Include "alltime" records
-		AND leaderboard_id LIKE $2`
-
-	rows, err := db.QueryContext(ctx, query, userID, groupID+":%")
-	if err != nil {
-		return nil, fmt.Errorf("failed to query latest leaderboard records: %w", err)
-	}
-	defer rows.Close()
-
-	records := make(map[evr.Symbol]map[string]map[evr.ResetSchedule]float64) //map[mode]map[statID]map[resetSchedule]float64
-	for rows.Next() {
-		var leaderboardID string
-		var score, subscore int64
-
-		err = rows.Scan(&leaderboardID, &score, &subscore)
-		if err != nil {
-			return nil, fmt.Errorf("failed to scan latest leaderboard record: %w", err)
-		}
-
-		meta, err := LeaderboardMetaFromID(leaderboardID)
-		if err != nil {
-			continue
-		}
-
-		if _, ok := records[meta.Mode]; !ok {
-			records[meta.Mode] = make(map[string]map[evr.ResetSchedule]float64)
-		}
-
-		if _, ok := records[meta.Mode][meta.StatName]; !ok {
-			records[meta.Mode][meta.StatName] = make(map[evr.ResetSchedule]float64)
-		}
-
-		records[meta.Mode][meta.StatName][meta.ResetSchedule] = ScoreToValue(score, subscore)
-
-	}
-
-	return records, nil
-}
-
-type PlayerLeaderboardHaystackRequest struct {
-	UserID        string            `json:"user_id"`
-	DiscordID     string            `json:"discord_id"`
-	GuildID       string            `json:"guild_id"`
-	GroupID       string            `json:"group_id"`
-	Mode          evr.Symbol        `json:"game_mode"`
-	ResetSchedule evr.ResetSchedule `json:"reset_schedule"`
-	StatName      string            `json:"stat_name"`
-}
-
-func LeaderboardHaystackRPC(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
-	request := &PlayerLeaderboardHaystackRequest{}
-	if err := parseRequest(ctx, payload, request); err != nil {
-		return "", err
-	}
-
-	var err error
-	if request.DiscordID != "" {
-		if request.UserID, err = GetUserIDByDiscordID(ctx, db, request.DiscordID); err != nil {
-			return "", fmt.Errorf("failed to get user ID by discord ID: %w", err)
-		}
-	}
-
-	if request.UserID == "" {
-		return "", runtime.NewError("No user ID specified", StatusInvalidArgument)
-	}
-
-	if request.GuildID != "" {
-		if request.GroupID, err = GetGroupIDByGuildID(ctx, db, request.GuildID); err != nil {
-			return "", fmt.Errorf("failed to get group ID by discord ID: %w", err)
-		}
-	}
-
-	if request.GroupID == "" {
-		return "", runtime.NewError("No group ID specified", StatusInvalidArgument)
-	}
-	if request.ResetSchedule == "" {
-		request.ResetSchedule = evr.ResetScheduleAllTime
-	}
-
-	id := LeaderboardMeta{
-		GroupID:       request.GroupID,
-		Mode:          request.Mode,
-		StatName:      request.StatName,
-		ResetSchedule: request.ResetSchedule,
-	}.ID()
-
-	limit := 10
-
-	records, err := nk.LeaderboardRecordsHaystack(ctx, id, request.UserID, limit, "", 0)
-	if err != nil {
-		logger.WithFields(map[string]interface{}{
-			"leaderboard_id": id,
-			"err":            err,
-		}).Error("Leaderboard record haystack error.")
-		return "", err
-	}
-
-	data, err := json.MarshalIndent(records, "", "  ")
-	if err != nil {
-		return "", fmt.Errorf("failed to marshal records: %w", err)
-	}
-
-	return string(data), nil
+	return response.String(), nil
 }
