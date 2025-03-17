d1e2d793985f6ae1b8b6d25a4ef9cceba866f8f8
Andrew Bates <a@sprock.io>
2025-03-17 03:13:11 +0000

    Improve matchmaker performance

--- server/evr_matchmaker.go
+++ server/evr_matchmaker.go
@@ -5,15 +5,12 @@ import (
 	"database/sql"
 	"encoding/json"
 	"log"
-	"math"
 	"os"
 	"slices"
 	"strings"
 	"time"
 
 	"github.com/heroiclabs/nakama-common/runtime"
-	"github.com/intinig/go-openskill/rating"
-	"github.com/intinig/go-openskill/types"
 	"github.com/samber/lo"
 	"go.uber.org/atomic"
 )
@@ -148,18 +145,19 @@ func (m *SkillBasedMatchmaker) processPotentialMatches(candidates [][]runtime.Ma
 	// Write the candidates to a json filed called /tmp/candidates.json
 	filterCounts := make(map[string]int)
 
-	// Filter out duplicates
-	filterCounts["duplicates"] = m.filterDuplicates(candidates)
-
-	// Filter out odd-sized teams
-	filterCounts["odd_sized"] = m.filterOddSizedMatches(candidates)
-
 	// Filter out players who are too far away from each other
 	filterCounts["max_rtt"] = m.filterWithinMaxRTT(candidates)
 
 	// Create a list of balanced matches with predictions
 
-	predictions := m.predictOutcomes(candidates)
+	predictions := predictMatchOutcomes(candidates)
+
+	f, err := os.Create("/tmp/predictions.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(predictions)
 
 	slices.SortStableFunc(predictions, func(a, b PredictedMatch) int {
 		if a.Size != b.Size {
@@ -173,56 +171,6 @@ func (m *SkillBasedMatchmaker) processPotentialMatches(candidates [][]runtime.Ma
 	madeMatches := m.assembleUniqueMatches(predictions)
 
 	return candidates, madeMatches, filterCounts
-}
-
-func (m *SkillBasedMatchmaker) filterDuplicates(candidates [][]runtime.MatchmakerEntry) int {
-
-	var (
-		seen      = make(map[string]struct{}) // Map to track seen candidate combinations
-		count     = 0
-		ticketIDs = make([]string, 0, 10)
-	)
-
-	for i, candidate := range candidates {
-		if candidate == nil {
-			continue
-		}
-		ticketIDs = ticketIDs[:0]
-
-		for _, e := range candidate {
-			ticketIDs = append(ticketIDs, e.GetTicket())
-		}
-		slices.Sort(ticketIDs)
-		key := strings.Join(ticketIDs, "")
-
-		// Check if the candidate has been seen before
-		if _, ok := seen[key]; ok {
-			count++
-			candidates[i] = nil
-			continue
-		}
-
-		// Mark the candidate as seen
-		seen[key] = struct{}{}
-	}
-
-	return count // Return the count of duplicates and the filtered candidates
-}
-
-func (m *SkillBasedMatchmaker) filterOddSizedMatches(candidates [][]runtime.MatchmakerEntry) int {
-	oddSizedCount := 0
-	for i := range candidates {
-		if candidates[i] == nil {
-			continue
-		}
-		if len(candidates[i])%2 != 0 {
-			oddSizedCount++
-			candidates[i] = nil
-			continue
-		}
-
-	}
-	return oddSizedCount
 }
 
 // Ensure that everyone in the match is within their max_rtt of a common server
@@ -271,169 +219,6 @@ OuterLoop:
 	}
 
 	return filteredCount
-}
-
-func (m *SkillBasedMatchmaker) predictOutcomes(candidates [][]runtime.MatchmakerEntry) []PredictedMatch {
-
-	// count the number of valid candidates
-	var validCandidates int
-	for _, c := range candidates {
-		if c != nil {
-			validCandidates++
-		}
-	}
-
-	type player struct {
-		entry   runtime.MatchmakerEntry
-		rating  types.Rating
-		ordinal float64
-	}
-
-	var (
-		predictions                  = make([]PredictedMatch, 0, validCandidates)
-		playersBySessionIDbyTicketID = make(map[string]map[string]player, 0)
-		ticketIDs                    = make([]string, 0, 8)
-		ticketStrengths              = make(map[string]float64, 10)
-	)
-
-	for _, c := range candidates {
-		if c == nil {
-			continue
-		}
-
-		candidateTicketSet := make(map[string]struct{}, len(c))
-		teamSize := len(c) / 2
-
-		for _, e := range c {
-			tID := e.GetTicket()
-
-			if _, ok := candidateTicketSet[tID]; !ok {
-				ticketIDs = append(ticketIDs, tID)
-			}
-			candidateTicketSet[tID] = struct{}{}
-
-			if _, ok := playersBySessionIDbyTicketID[tID]; !ok {
-				playersBySessionIDbyTicketID[tID] = make(map[string]player, 0)
-			}
-
-			// Add the player once
-			if _, ok := playersBySessionIDbyTicketID[tID][e.GetPresence().GetSessionId()]; !ok {
-
-				// Extract the player's rating
-				r := types.Rating{Mu: 25.0, Sigma: 8.333}
-				if mu, ok := e.GetProperties()["rating_mu"].(float64); ok {
-					if sigma, ok := e.GetProperties()["rating_sigma"].(float64); ok {
-						r.Mu = mu
-						r.Sigma = sigma
-					}
-				}
-				playersBySessionIDbyTicketID[tID][e.GetPresence().GetSessionId()] = player{
-					entry:   e,
-					rating:  r,
-					ordinal: rating.Ordinal(r),
-				}
-			}
-		}
-
-		// Calculate the strength of each ticket
-		ticketIDs = ticketIDs[:0]
-		for tID := range candidateTicketSet {
-
-			// Add the ticket to the list
-			ticketIDs = append(ticketIDs, tID)
-
-			// Calculate the strength of the ticket (once)
-			if _, ok := ticketStrengths[tID]; !ok {
-				count := 0
-				for _, p := range playersBySessionIDbyTicketID[tID] {
-					ticketStrengths[tID] += p.rating.Mu
-					count++
-				}
-				if count == 0 {
-					ticketStrengths[tID] = 0
-				} else {
-					ticketStrengths[tID] /= float64(count)
-				}
-			}
-		}
-
-		// Sort the ticketIDs by size, largest first, then by strength (descending)
-		slices.SortStableFunc(ticketIDs, func(a, b string) int {
-			if a == b {
-				return 0
-			}
-			if len(playersBySessionIDbyTicketID[a]) != len(playersBySessionIDbyTicketID[b]) {
-				return len(playersBySessionIDbyTicketID[b]) - len(playersBySessionIDbyTicketID[a])
-			}
-			return int(ticketStrengths[b] - ticketStrengths[a])
-		})
-
-		type team struct {
-			sessionIDs map[string]struct{}
-			ratings    []types.Rating
-			strength   float64
-			length     int
-			ordinal    float64
-		}
-
-		teams := [2]team{
-			{
-				sessionIDs: make(map[string]struct{}, teamSize),
-				ratings:    make([]types.Rating, 0, teamSize),
-				strength:   0,
-				length:     0,
-			},
-			{
-				sessionIDs: make(map[string]struct{}, teamSize),
-				ratings:    make([]types.Rating, 0, teamSize),
-				strength:   0,
-				length:     0,
-			},
-		}
-
-		// Assign players to teams
-		for _, tID := range ticketIDs {
-			teamIdx := 0
-			ticketLength := len(playersBySessionIDbyTicketID[tID])
-			if teams[0].length+ticketLength >= teamSize && (teams[1].length+ticketLength < teamSize || teams[0].strength > teams[1].strength) {
-				teamIdx = 1
-			}
-
-			for _, p := range playersBySessionIDbyTicketID[tID] {
-				teams[teamIdx].sessionIDs[p.entry.GetPresence().GetSessionId()] = struct{}{}
-				teams[teamIdx].ratings = append(teams[teamIdx].ratings, p.rating)
-				teams[teamIdx].strength += p.rating.Mu
-				teams[teamIdx].length += 1
-			}
-
-		}
-
-		// Sort so that team1 (blue) is the stronger team
-		if teams[0].strength < teams[1].strength {
-			teams[0], teams[1] = teams[1], teams[0]
-		}
-		for i := 0; i < 2; i++ {
-			teams[i].ordinal = rating.TeamOrdinal(types.TeamRating{Team: teams[i].ratings})
-		}
-		// Sort the entries by the team they are on
-		slices.SortStableFunc(c, func(a, b runtime.MatchmakerEntry) int {
-			if _, ok := teams[0].sessionIDs[a.GetPresence().GetSessionId()]; ok {
-				return -1
-			}
-			return 1
-		})
-
-		predictions = append(predictions, PredictedMatch{
-			Candidate:    c,
-			Draw:         rating.PredictDraw([]types.Team{teams[0].ratings, teams[1].ratings}, nil),
-			Size:         len(c),
-			TeamOrdinalA: teams[0].strength,
-			TeamOrdinalB: teams[1].strength,
-			OrdinalDelta: math.Abs(teams[0].ordinal - teams[1].ordinal),
-		})
-	}
-
-	return predictions
 }
 
 func (m *SkillBasedMatchmaker) assembleUniqueMatches(sortedCandidates []PredictedMatch) [][]runtime.MatchmakerEntry {
--- server/evr_matchmaker_sorting.go
+++ server/evr_matchmaker_sorting.go
@@ -3,10 +3,13 @@ package server
 import "github.com/heroiclabs/nakama-common/runtime"
 
 type PredictedMatch struct {
-	Candidate    []runtime.MatchmakerEntry `json:"match"`
-	Draw         float64                   `json:"draw"`
-	Size         int                       `json:"size"`
-	TeamOrdinalA float64                   `json:"team_ordinal_a"`
-	TeamOrdinalB float64                   `json:"team_ordinal_b"`
-	OrdinalDelta float64                   `json:"ordinal_delta"`
+	Candidate           []runtime.MatchmakerEntry `json:"match"`
+	Draw                float64                   `json:"draw"`
+	Size                int                       `json:"size"`
+	TeamRankPercentileA float64                   `json:"rank_percentile_a"`
+	TeamRankPercentileB float64                   `json:"rank_percentile_b"`
+	RankPercentileDelta float64                   `json:"rank_percentile_delta"`
+	TeamOrdinalA        float64                   `json:"team_ordinal_a"`
+	TeamOrdinalB        float64                   `json:"team_ordinal_b"`
+	OrdinalDelta        float64                   `json:"ordinal_delta"`
 }
