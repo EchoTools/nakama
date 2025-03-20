6e66ae5d338d3eea27c9acc346e2081d7d1fbdec
Andrew Bates <a@sprock.io>
2025-03-20 01:56:49 +0000

    Move update to separate file

--- server/evr_pipeline_login.go
+++ server/evr_pipeline_login.go
@@ -1113,49 +1113,6 @@ func (p *EvrPipeline) processUserServerProfileUpdate(ctx context.Context, logger
 	return p.updatePlayerStats(ctx, playerInfo.UserID, groupIDStr, playerInfo.DisplayName, payload.Update, label.Mode)
 }
 
-func (p *EvrPipeline) updatePlayerStats(ctx context.Context, userID, groupID, displayName string, update evr.ServerProfileUpdate, mode evr.Symbol) error {
-	var stats evr.Statistics
-
-	// Select the correct statistics based on the mode
-	switch mode {
-	case evr.ModeArenaPublic:
-		if update.Statistics.Arena == nil {
-			return fmt.Errorf("missing arena statistics")
-		}
-		stats = update.Statistics.Arena
-	case evr.ModeCombatPublic:
-		if update.Statistics.Combat == nil {
-			return fmt.Errorf("missing combat statistics")
-		}
-		stats = update.Statistics.Combat
-	default:
-		return fmt.Errorf("unknown mode: %s", mode)
-	}
-
-	// Get the players existing statistics
-	prevPlayerStats, _, err := PlayerStatisticsGetID(ctx, p.db, p.nk, userID, groupID, []evr.Symbol{mode}, mode)
-	if err != nil {
-		return fmt.Errorf("failed to get player statistics: %w", err)
-	}
-	g := evr.StatisticsGroup{
-		Mode:          mode,
-		ResetSchedule: evr.ResetScheduleAllTime,
-	}
-
-	// Use defaults if the player has no existing statistics
-	prevStats, ok := prevPlayerStats[g]
-	if !ok {
-		prevStats = evr.NewServerProfile().Statistics[g]
-	}
-
-	entries, err := StatisticsToEntries(userID, displayName, groupID, mode, prevStats, stats)
-	if err != nil {
-		return fmt.Errorf("failed to convert statistics to entries: %w", err)
-	}
-
-	return p.statisticsQueue.Add(entries)
-}
-
 func (p *EvrPipeline) otherUserProfileRequest(ctx context.Context, logger *zap.Logger, session *sessionWS, in evr.Message) error {
 	request := in.(*evr.OtherUserProfileRequest)
 
