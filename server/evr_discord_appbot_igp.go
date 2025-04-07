f60cef27bb1682ae166c922b6e7617ea44859dc5
Andrew Bates <a@sprock.io>
2025-04-07 18:53:45 +0000

    Add IGP

---
+++ server/evr_discord_appbot_igp.go
@@ -0,0 +1,81 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/gofrs/uuid/v5"
+	"github.com/heroiclabs/nakama-common/runtime"
+	"go.uber.org/zap"
+)
+
+func (d *DiscordAppBot) handleInGamePanel(logger runtime.Logger, s *discordgo.Session, i *discordgo.InteractionCreate, user *discordgo.User, member *discordgo.Member, userID string, groupID string) error {
+	ctx := context.Background()
+	// find the user in the streams
+	presences, err := d.nk.StreamUserList(StreamModeService, userID, "", StreamLabelLoginService, false, true)
+	if err != nil {
+		return fmt.Errorf("failed to list igp users: %w", err)
+	}
+
+	if len(presences) == 0 {
+		return errors.New("you must be in the game to use this command")
+	}
+
+	var presence runtime.Presence
+	for _, p := range presences {
+		if p.GetUserId() == userID {
+			presence = p
+			break
+		}
+	}
+
+	_nk := d.nk.(*RuntimeGoNakamaModule)
+
+	session := _nk.sessionRegistry.Get(uuid.FromStringOrNil(presence.GetSessionId()))
+	if session == nil {
+		return errors.New("you must be in the game to use this command")
+	}
+
+	params, ok := LoadParams(session.Context())
+	if !ok {
+		return errors.New("failed to load params")
+	}
+
+	wasGold := params.isGoldNameTag.Toggle()
+
+	content := "You are now using the gold name tag."
+	if wasGold {
+		content = "You are no longer using the gold name tag."
+	}
+	_ = content
+
+	components, err := d.ModPanelMessageEmbed(ctx, logger, d.nk, member.User.ID)
+	if err != nil {
+		if err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Flags:   discordgo.MessageFlagsEphemeral,
+				Content: "Failed to load mod panel: " + err.Error(),
+			},
+		}); err != nil {
+			logger.Error("Failed to send mod panel error message", zap.Error(err))
+			return nil
+		}
+	}
+
+	response := &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Flags:   discordgo.MessageFlagsEphemeral,
+			Content: "Select a player to kick",
+			Components: []discordgo.MessageComponent{
+				discordgo.ActionsRow{
+					Components: components,
+				},
+			},
+		},
+	}
+	return s.InteractionRespond(i.Interaction, response)
+}
---
+++ server/evr_discord_appbot_party_status.go
@@ -0,0 +1,210 @@
+package server
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"slices"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/heroiclabs/nakama-common/runtime"
+	"go.uber.org/thriftrw/ptr"
+	"go.uber.org/zap"
+)
+
+func (d *DiscordAppBot) handlePartyStatus(logger runtime.Logger, s *discordgo.Session, i *discordgo.InteractionCreate, user *discordgo.User, member *discordgo.Member, userID string, groupID string) error {
+	ctx := context.Background()
+	nk := d.nk
+	// Check if this user is online and currently in a party.
+	groupName, partyUUID, err := GetLobbyGroupID(ctx, d.db, userID)
+	if err != nil {
+		return fmt.Errorf("failed to get party group ID: %w", err)
+	}
+
+	if groupName == "" {
+		return s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Flags:   discordgo.MessageFlagsEphemeral,
+				Content: "You do not have a party group set. use `/party group` to set one.",
+			},
+		})
+	}
+
+	go func() {
+		// "Close" the embed on return
+		defer func() {
+			if _, err := s.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{
+				Content: ptr.String("Party Status (Expired)"),
+				Embeds: &[]*discordgo.MessageEmbed{{
+					Title: "Party has been empty for more than 2 minutes.",
+				}},
+			}); err != nil {
+				logger.Error("Failed to edit interaction response", zap.Error(err))
+			}
+		}()
+
+		embeds := make([]*discordgo.MessageEmbed, 4)
+		// Create all four embeds for the party members.
+		for i := range embeds {
+			embeds[i] = &discordgo.MessageEmbed{
+				Title: "*Empty Slot*",
+			}
+		}
+
+		partyStr := partyUUID.String()
+
+		var message *discordgo.Message
+		var err error
+		var members []runtime.Presence
+		var lastDiscordIDs []string
+		var memberCache = make(map[string]*discordgo.Member)
+
+		updateInterval := 3 * time.Second
+		ticker := time.NewTicker(1 * time.Second)
+		defer ticker.Stop()
+
+		emptyTimer := time.NewTimer(120 * time.Second)
+		defer emptyTimer.Stop()
+
+		// Loop until the party is empty for 2 minutes.
+		for {
+
+			select {
+			case <-emptyTimer.C:
+
+				// Check if the party is still empty.
+				if len(members) > 0 {
+					emptyTimer.Reset(120 * time.Second)
+					continue
+				}
+				return
+
+			case <-ticker.C:
+				ticker.Reset(updateInterval)
+			}
+
+			// List the party members.
+			members, err = nk.StreamUserList(StreamModeParty, partyStr, "", d.pipeline.node, false, true)
+			if err != nil {
+				logger.Error("Failed to list stream users", zap.Error(err))
+				return
+			}
+
+			emptyState := &LobbySessionParameters{}
+
+			matchmakingStates := make(map[string]*LobbySessionParameters, len(members)) // If they are matchmaking
+			for _, p := range members {
+				matchmakingStates[p.GetUserId()] = emptyState
+			}
+
+			discordIDs := make([]string, 0, len(members))
+			for _, p := range members {
+				discordIDs = append(discordIDs, d.cache.UserIDToDiscordID(p.GetUserId()))
+			}
+
+			slices.Sort(discordIDs)
+
+			groupID := d.cache.GuildIDToGroupID(i.GuildID)
+
+			// List who is matchmaking
+			currentlyMatchmaking, err := nk.StreamUserList(StreamModeMatchmaking, groupID, "", "", false, true)
+			if err != nil {
+				logger.Error("Failed to list stream users", zap.Error(err))
+				return
+			}
+
+			idleColor := 0x0000FF // no one is matchmaking
+
+			for _, p := range currentlyMatchmaking {
+				if _, ok := matchmakingStates[p.GetUserId()]; ok {
+
+					// Unmarshal the user status
+					if err := json.Unmarshal([]byte(p.GetStatus()), matchmakingStates[p.GetUserId()]); err != nil {
+						logger.Error("Failed to unmarshal user status", zap.Error(err))
+						continue
+					}
+
+					idleColor = 0xFF0000 // Someone is matchmaking
+				}
+			}
+
+			// Update the embeds with the current party members.
+			for j := range embeds {
+				if j < len(discordIDs) {
+
+					// Set the display name
+					var member *discordgo.Member
+					var ok bool
+
+					if member, ok = memberCache[discordIDs[j]]; !ok {
+						if member, err = d.cache.GuildMember(i.GuildID, discordIDs[j]); err != nil {
+							logger.Error("Failed to get guild member", zap.Error(err))
+							embeds[j].Title = "*Unknown User*"
+							continue
+						} else if member != nil {
+							memberCache[discordIDs[j]] = member
+						} else {
+							embeds[j].Title = "*Unknown User*"
+							continue
+						}
+					}
+					displayName := member.DisplayName()
+					if displayName == "" {
+						displayName = member.User.Username
+					}
+					embeds[j].Title = displayName
+					embeds[j].Thumbnail = &discordgo.MessageEmbedThumbnail{
+						URL: member.User.AvatarURL(""),
+					}
+					userID := d.cache.DiscordIDToUserID(member.User.ID)
+					if state, ok := matchmakingStates[userID]; ok && state != emptyState {
+
+						embeds[j].Color = 0x00FF00
+						embeds[j].Description = "Matchmaking"
+					} else {
+
+						embeds[j].Color = idleColor
+						embeds[j].Description = "In Party"
+					}
+				} else {
+					embeds[j].Title = "*Empty Slot*"
+					embeds[j].Color = 0xCCCCCC
+				}
+			}
+
+			if message == nil {
+
+				// Send the initial message
+				if err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Flags:  discordgo.MessageFlagsEphemeral,
+						Embeds: embeds,
+					},
+				}); err != nil {
+					logger.Error("Failed to send interaction response", zap.Error(err))
+					return
+				}
+
+			} else if slices.Equal(discordIDs, lastDiscordIDs) {
+				// No changes, skip the update.
+				continue
+			}
+
+			lastDiscordIDs = discordIDs
+
+			// Edit the message with the updated party members.
+			if message, err = s.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{
+				Content: ptr.String("Party Status"),
+				Embeds:  &embeds,
+			}); err != nil {
+				logger.Error("Failed to edit interaction response", zap.Error(err))
+				return
+			}
+
+		}
+	}()
+	return nil
+}
