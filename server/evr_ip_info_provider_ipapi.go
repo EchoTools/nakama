b30d8e1989b5c635316961e91eed9ccb7ee7cec9
TechSide <wienerwolf77@gmail.com>
2025-04-19 11:05:11 +1200

    comment out local fix, unsure if needed for prod

--- server/evr_ip_info_provider_ipapi.go
+++ server/evr_ip_info_provider_ipapi.go
@@ -168,7 +168,8 @@ func (s *ipapiClient) load(ip string) (*IPAPIResponse, error) {
 }
 
 func (s *ipapiClient) store(ip string, result *IPAPIResponse) error {
-	return nil
+	// Uncomment this to fix local IPs for testing.
+	// return nil
 	data, err := json.Marshal(result)
 	if err != nil {
 		return fmt.Errorf("failed to marshal data: %v", err)
