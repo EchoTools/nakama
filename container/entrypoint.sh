863fbf222d9fe145705f673841f9406d809b1ebd
TechSide <68258126+techsideofficial@users.noreply.github.com>
2025-04-19 13:57:40 +1200

    Delete container/entrypoint.sh
    Signed-off-by: TechSide <68258126+techsideofficial@users.noreply.github.com>

--- container/entrypoint.sh
+++
@@ -1,1 +0,0 @@
-/nakama-app/nakama --database.address "postgresql://techside:yfy1Rzv7OPTsMx016MOIFw@arparec-us-central-5949.jxf.gcp-us-central1.cockroachlabs.cloud:26257/nakama-evr-dev?sslmode=verify-full" --config /nakama-app/data/local.yml
