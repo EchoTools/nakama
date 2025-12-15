0235687c852e6e3dcc841a37b9919eef9766e24a
Andrew Bates <a@sprock.io>
2025-12-15 16:51:03 -0600

    Fix/no-sessions-found-when-kicking-player (#198)
    * Improve logging for match label retrieval failure by including match ID
    
    * Record session duration in metrics after login request
    
    * Fix nil member handling in handleLinkHeadset and add corresponding test
    
    * Update server/evr_pipeline_login.go
    
    Signed-off-by: Andrew Bates <a@sprock.io>

--- server/evr_pipeline_login.go
+++ server/evr_pipeline_login.go
@@ -168,6 +168,12 @@ func (p *EvrPipeline) loginRequest(ctx context.Context, logger *zap.Logger, sess
 		gameSettings.RemoteLogRichPresence = true
 		gameSettings.RemoteLogMetrics = true
 	}
+	metricsTags := params.MetricsTags()
+	go func(metricsTags map[string]string) {
+		<-session.Context().Done()
+		// Record the session duration
+		p.nk.metrics.CustomGauge("session_duration_seconds", metricsTags, time.Since(timer).Seconds())
+	}(metricsTags)
 
 	return session.SendEvr(
 		evr.NewLoginSuccess(session.id, request.XPID),
